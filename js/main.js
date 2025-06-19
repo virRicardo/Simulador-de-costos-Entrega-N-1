422f3c842d91a1f9c9652a135fe2494db0802dd4
virRicardo <victoriaricardo3@gmail.com>
2025-06-19 14:15:56 -0300

    primer commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 const precioNaftaPorKm = 100;
 const precioPorNocheHotel = 2000;
 
-let totalKm = 0;
-let totalNoches = 0;
-let costoTotalViaje = 0;
-let costoNafta = 0;
-let costoHotel = 0;
-
+let simulacionDeViajes = [];
 
 function obtenerDatos (){
     alert("Bienvenidos a éste simulador de costo de viaje que le ayudará a planificar sus proximas vacaciones!")
     console.log("---Bienvenidos a éste simulador de costo de viaje que le ayudará a planificar sus proximas vacaciones!---");
+
+    let totalKm = 0;
+    let totalNoches = 0;
 
     let inputDistancia = prompt("Por favor ingrese la cantidad total de km que realizará");
     totalKm = parseFloat(inputDistancia);
@@ -29,8 +27,12 @@ function obtenerDatos (){
         inputNoches = prompt("Ingrese la cantidad de noches que se hospedará en el hotel");
         totalNoches = parseInt(inputNoches);
     }
+
+    return{
+        km: totalKm,
+        noches: totalNoches
+    };
 }
-obtenerDatos();
 
 function calcularTotalCombustible (distancia, precioCombustible){
     return distancia * precioCombustible;
@@ -44,14 +46,66 @@ function costoTotal(combustible, alojamiento){
     return combustible + alojamiento;
 }
 
-costoNafta = calcularTotalCombustible(totalKm, precioNaftaPorKm);
-costoHotel = calcualarTotalHotel(totalNoches, precioPorNocheHotel);
-costoTotalViaje = costoTotal( costoNafta, costoHotel);
+function realizarSimulacion(){
+    const datosViaje = obtenerDatos();
 
-console.log("---Resumen del viaje---");
-console.log(`Distancia total del viaje: ${totalKm} Km`);
-console.log(`Días totales de hotel: ${totalNoches} noches`);
-console.log(`Costo total de combustible: $ ${costoNafta}`);
-console.log(`Costo total de Hotel: $ ${costoHotel}`);
-console.log(`Costo total de viaje: $ ${costoTotalViaje}`);
-alert(`Costo total de viaje: $ ${costoTotalViaje}`);
+    const costoNafta = calcularTotalCombustible(datosViaje.km, precioNaftaPorKm);
+    const costoHotel = calcualarTotalHotel(datosViaje.noches, precioPorNocheHotel);
+    const costoTotalViaje = costoTotal(costoNafta,costoHotel);
+
+    const resumenSimulacion ={
+        id: simulacionDeViajes.length +1,
+        totalKm : datosViaje.km,
+        totalNoches : datosViaje.noches,
+        costoNafta : costoNafta,
+        costoHotel : costoHotel,
+        costoTotalViaje : costoTotalViaje
+    };
+
+    simulacionDeViajes.push(resumenSimulacion);
+
+    console.log("---Resumen simulación de viaje---");
+    console.log(`Simulación #${resumenSimulacion.id}`);
+    console.log(`Distancia total de viaje ${resumenSimulacion.totalKm} km`);
+    console.log(`Días totales en hotel ${resumenSimulacion.totalNoches}`);
+    console.log(`Costo total de combustible ${resumenSimulacion.costoNafta}`);
+    console.log(`Costo total hotel ${resumenSimulacion.costoHotel}`);
+    console.log(`Costo toal de viaje ${resumenSimulacion.costoTotalViaje}`);
+    alert(`Costo total de viaje $${resumenSimulacion.costoTotalViaje}`);
+}
+
+let continuar = true;
+while(continuar){
+    realizarSimulacion();
+
+    let respuesta = prompt("Desea realizar otra simulación? Escriba si o no por favor").toLocaleLowerCase();
+    if(respuesta !==`si`){
+        continuar = false;
+    }
+}
+
+console.log("\n Historial de simulaciones");
+if(simulacionDeViajes.length === 0){
+    console.log("Aún no realizo ninguna simulación");
+}else{
+    simulacionDeViajes.forEach(simulacion => {
+        console.log(`\n Simulacion #${simulacion.id}`);
+        console.log(`\n Km ${simulacion.totalKm}`);
+        console.log(`\n Noches ${simulacion.totalNoches}`);
+        console.log(`\n Costo nafta ${simulacion.costoNafta}`);
+        console.log(`\n Costo hotel ${simulacion.costoHotel}`);
+        console.log(`Costo total: $ ${simulacion.costoTotalViaje}`);
+    });
+}
+
+console.log("--------------------------------------------------------");
+
+
+
+
+
+
+
+
+
+
